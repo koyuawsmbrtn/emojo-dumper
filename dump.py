ad2a472fa2a31e940b995ee976965c56c2fc39f7
koyu <me@koyu.space>
2024-10-23 13:41:26 +0200

    Add ability to download a single pack only

--- dump.py
+++ dump.py
@@ -5,11 +5,17 @@ from dotenv import load_dotenv
 instance = os.getenv("INSTANCE")
 if not instance:
     raise ValueError("INSTANCE is not set! Make sure the environment variable or .env exists.")
+category = os.getenv("CATEGORY")
 
 r = requests.get("https://"+instance+"/api/v1/custom_emojis")
 j = r.json()
 
 for item in j:
     if item["visible_in_picker"] == True:
-        extension = item["url"].split(".")[len(item["url"].split(".")) - 1]
-        os.system("wget -O "+item["shortcode"]+"."+extension+" "+item["url"])
+        if category:
+            if item["category"] == category:
+                extension = item["url"].split(".")[len(item["url"].split(".")) - 1]
+                os.system("wget -O "+item["shortcode"]+"."+extension+" "+item["url"])
+        else:
+            extension = item["url"].split(".")[len(item["url"].split(".")) - 1]
+            os.system("wget -O "+item["shortcode"]+"."+extension+" "+item["url"])
